27951b332e2b1b4b8d5c057d0bd38e1159275874
Dan Doyle <daldoyle@daldoyle-dev-6.grnoc.iu.edu>
2016-04-19 12:55:10 +0000

    ISSUE=761 adding upgrade file

---
+++ lib/GRNOC/TSDS/Upgrade/1_5_1.pm
@@ -0,0 +1,24 @@
+package GRNOC::TSDS::Upgrade::1_5_1;
+
+use strict;
+use warnings;
+
+use GRNOC::TSDS::Install;
+use GRNOC::TSDS::MongoDB;
+use Tie::IxHash;
+use Data::Dumper;
+
+use constant PREVIOUS_VERSION => '1.5.0';
+
+sub upgrade {
+
+    my ( $self, $upgrade ) = @_;
+
+    ### UPGRADE CODE GOES HERE ###
+
+    my $mongo = $upgrade->mongo_root;
+
+    return 1;
+}
+
+1;
