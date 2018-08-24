1fe76edf9371d934b5dfd1b6496b0933a1ed8581
Sreemukha <sreemukha.mail@gmail.com>
2018-08-24 17:46:43 +0000

    ISSUE=8189 PROJ=160 Adding script to add/delete metafields/indexes

--- bin/tsds_adjust_metafields.pl
+++ bin/tsds_adjust_metafields.pl
@@ -32,49 +32,65 @@ my $admin_client = GRNOC::WebService::Client->new(uid => $user,
                                                   passwd => $pass,
                                                   url => $admin_url);
 
-#delete_fields();
-#add_fields();
+delete_fields();
+add_fields();
 
-#           #       
-#           #
-# FUNCTIONS #
-#           #
-#           #
+########################
+#                      #
+#       FUNCTIONS      # 
+#                      #
+######################## 
 
 sub unset_old_fields {
     my $measurement_coll = shift;
     my $unset_field = shift; 
-    my $return = $measurement_coll->update({}, {'$unset' => {"$unset_field" => 1}}, {'multiple' => 1});
+    my $return = eval { 
+        $measurement_coll->update({"$unset_field" => {'$exists' => 1}}, {'$unset' => {"$unset_field" => 1}}, {'multiple' => 1});
+    };
+    if($@) {
+        die "Error unsetting the field - $unset_field, " . $@;
+    }
+    print "Unsetting...$unset_field, " . Dumper($return);
 }
 
 sub remove_index {
     my $measurement_coll = shift;
     my $omission = shift;
-    $measurement_coll->drop_index($omission);
+    my $return = eval { 
+        $measurement_coll->drop_index($omission);
+    };
+    if($@){
+       die "Error Dropping the index - $omission, " . $@;
+    }
+    print "Drop Index...$omission, " . Dumper($return);
 }
 
 sub delete_fields {
     my $db = $mongo->get_database('interface');
     my $measurement_coll = $db->get_collection('measurements');
      
-    my @deletions = ('pop.owner', 'pop.hands_and_eyes', 'pop.role', 'pop.pop_id', 'circuit.circuit_id', 'network_id', 'entity.entity_id', 'node_id');
+    my @deletions = ('pop.owner', 'pop.hands_and_eyes', 'pop.role', 'pop.pop_id', 'network_id', 'entity.entity_id', 'node_id');
     
     foreach my $omission (@deletions) {
         my $deleted = $admin_client->delete_meta_fields(measurement_type => "interface", name => $omission);
-        
-        if (defined $deleted->{error_text} and $deleted->{'error_text'} =~ m/does not exist/) {
+        die "Couldn't delete the field\n" if(!defined $deleted);
+    
+        if (defined $deleted->{'error_text'} and $deleted->{'error_text'} =~ m/does not exist/) {
             print "Skipping drop_index and unset_fields\n";
             next;
         }
+        print "Deleted metafield, $omission\n";
+        print "Unsetting the field...\n";     
 
-        print "Dropping index and unsetting the field...\n"; 
+        # unset the field now
+        unset_old_fields($measurement_coll, $omission);
+
+        print "Dropping index...\n"; 
 
         # drop the index now
         my $om = $omission.'_1';
         remove_index($measurement_coll, $om);
         
-        # unset the field now
-        unset_old_fields($measurement_coll, $omission);
     }
 }
 
@@ -82,5 +98,11 @@ sub add_fields {
     my @additions = ('service.entity_type', 'circuit.carrier_type', 'circuit.customer_type');
     foreach my $addtn (@additions){
         my $add = $admin_client->add_meta_field(measurement_type => 'interface', name => $addtn);
+        die "Couldn't add the field\n" if(!defined $add);
+        if (defined $add->{'error_text'} and $add->{'error_text'} =~ m/already exists/){
+            print "Field already exists\n";
+            next;
+	    }
+        print "Added new metafield, $addtn\n";       
     }
 }
